f7f94496ef71d6c06784539bb774db7b837a7cda
Alejandra López <alejandralopezfajardo11@gmail.com>
2026-09-25 00:26:02 -0600

    Update and rename config.example.js to config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: "https://aeidlaqydofrsaflzhoo.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFlaWRsYXF5ZG9mcnNhZmx6aG9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAyODc5MzcsImV4cCI6MjEwNTg2MzkzN30.KImkvANe8lkrVHE_oKuqNQ32rrBlHciblucnTZaUP0s"
+};
